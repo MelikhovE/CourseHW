8714cda86712fab0395b4bb15685d14e2847ce0f
Egor <melikhovegor10@gmail.com>
2023-04-17 21:00:47 +0300

    #3 Добавил метод вычисления расстояния между точками

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ class Point:
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
+
+    def distance(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
